1b2408eeb8689f29393ea945866a3581137e0b54
희호 <kim21670@naver.com>
2026-05-20 23:49:08 +0900

    feat: ECharts 공통 컴포넌트 및 Lifecycle 처리 추가

---
+++ src/components/BaseChart.vue
@@ -0,0 +1,128 @@
+<template>
+  <div ref="chart" :style="props.style"></div>
+</template>
+<script setup lang="ts">
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
+import * as echarts from 'echarts/core';
+import { BarChart, LineChart } from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent,
+} from 'echarts/components';
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+import type { BarSeriesOption, LineSeriesOption } from 'echarts/charts';
+import type {
+  TitleComponentOption,
+  TooltipComponentOption,
+  GridComponentOption,
+  DatasetComponentOption,
+  LegendComponentOption,
+} from 'echarts/components';
+import type { ComposeOption } from 'echarts/core';
+
+type ECOption = ComposeOption<
+  | BarSeriesOption
+  | LineSeriesOption
+  | TitleComponentOption
+  | TooltipComponentOption
+  | GridComponentOption
+  | DatasetComponentOption
+  | LegendComponentOption
+>;
+
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  BarChart,
+  LineChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+  LegendComponent,
+]);
+
+interface ChartProps {
+  style?: {
+    width: string;
+    height: string;
+  };
+  title?: string;
+  legend?: ECOption['legend'];
+  tooltip?: ECOption['tooltip'];
+  xAxis?: ECOption['xAxis'];
+  yAxis?: ECOption['yAxis'];
+  series?: ECOption['series'];
+}
+
+const props = withDefaults(defineProps<ChartProps>(), {
+  style: () => ({
+    width: '100%',
+    height: '400px',
+  }),
+  title: '',
+  legend: () => ({
+    data: ['Series 1'],
+  }),
+  tooltip: () => ({}),
+  xAxis: () => ({
+    type: 'category',
+    data: [],
+  }),
+  yAxis: () => ({
+    type: 'value',
+  }),
+  series: () => [
+    {
+      name: 'Series 1',
+      type: 'line',
+      data: [],
+    },
+  ],
+});
+
+const chart = ref<HTMLElement | null>(null);
+const chartInstance = ref<echarts.ECharts | null>(null);
+const option = computed<ECOption>(() => ({
+  title: {
+    text: props.title,
+  },
+  legend: props.legend,
+  tooltip: props.tooltip,
+  xAxis: props.xAxis,
+  yAxis: props.yAxis,
+  series: props.series,
+}));
+
+watch(option, (newOption) => {
+  chartInstance.value?.setOption(newOption);
+});
+
+const handleResize = () => {
+  chartInstance.value?.resize();
+};
+
+onMounted(() => {
+  if (!chart.value) return;
+
+  chartInstance.value = echarts.init(chart.value);
+
+  chartInstance.value.setOption(option.value);
+
+  window.addEventListener('resize', handleResize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+
+  chartInstance.value?.dispose();
+  chartInstance.value = null;
+});
+</script>
